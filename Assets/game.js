04f66092944baca923b09c5ecf0db4827f5a789d
Andrew Mayo <andrewmayo@Andrews-MacBook-Pro.local>
2018-12-15 20:04:38 -0500

    finished

--- Assets/game.js
+++ Assets/game.js
@@ -1,90 +1,101 @@
-//Pseudo Code//
+let actor = ["Seth Rogen", "Tom Cruise", "Richard Gere", "Jason Segel"];
 
 
+function displayActor (){
 
+var actor = $(this).attr("data-name");
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + actor + "&api_key=wH45n8pCbWdmGgONQpIimBMLweZWUWKQ";
 
-
-$(document).ready(function(){
-$("button").on("click", function() {
-
-var person = $(this).attr("data-person");
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-  person + "&api_key=dc6zaTOxFJmzC&limit=10";
-  
 $.ajax({
-  url: queryURL,
-  method: "GET"
-})
-  .then(function(response) {
-    var results = response.data;
-    console.log(results);
-  
-    for (var i = 0; i < results.length; i++) {
-    
-    var gifDiv = $("<div>");
-  
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+
+    let results = response.data;
+
+    $("#gif-view").empty();
+
+    for (let i = 0; i < 10; i++) {
+    var div = $("<div>");
     var rating = results[i].rating;
-  
     var p = $("<p>").text("Rating: " + rating);
-  
-    var activeGif = $("<img class='gif'>");
+    var img = $("<img>");
 
-    activeGif.attr("src", results[i].images.fixed_height_still.url);
-    activeGif.attr("data-state", "data-still");
+    div.attr("class", "card_gifIn" );
 
+        img.attr("src", results[i].images.fixed_height_still.url);
 
+        img.attr("data-still", results[i].images.fixed_height_still.url);
 
-    console.log(activeGif)
-    
-    gifDiv.prepend(p);
-    gifDiv.prepend(activeGif);
-  
-    $("#gifs-appear-here").prepend(gifDiv);
+        img.attr("data-animate", results[i].images.fixed_height.url);
 
+        img.data("state", "still" );
+
+        img.attr("class", "movingGif");
+
+    div.append(p);
+
+    div.append(img);
+
+    $("#gif-view").prepend(div);
     }
-//===================== CLICK FUNCTION =====================//
-    $(".gif").on("click", function(){
-        let state = $(this).data("state");
-        console.log(state);
 
-        if (state === "still") {
-            $(this).attr("src", $(this).attr("data-animate"));
-            $(this).data("state", "animate");
-        } else {
-            activeGif.attr("src", results[i].images.fixed_height.url )
-            $(this).data("state", "still");
-        }
-    })
-})
+    $(".movingGif").on("click", function () {
 
+    var state = $(this).data("state");
 
-
-
-
-// display gif function
-
-// function for render buttons hw click json
-
-// add gif button with search bar hw click json
-
-//
-
-})
-
-  
-// $(".gif").on("click", function(){
-//     var state = $(this).attr('data-state');
-//     console.log(state);
-    
-    
-
-//     if(state === "animate"){
-//         $(this).attr("src", $(this).attr('data-still'));
-//         $(this).attr("data-state", "animate");
-//     }else{
-//         $(this).attr("src", $(this).attr("data-animate"));
-//         $(this).attr("data-state", "animate");
-//     }
-// })
-
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).data("state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).data("state", "still");
+        
+    }
 });
+});
+}
+
+function showButtons () {
+    $('#buttons').empty();
+
+    for (let i = 0; i < actor.length; i++) {
+        var b = $("<button>");
+
+        b.addClass("btn btn-outline-danger showButtons");
+
+        b.attr("data-name", actor[i]);
+
+        b.text(actor[i]);
+
+        $("#buttons").append(b);
+        
+    }
+}
+
+
+$("#add-actor").on("click", function(event) {
+    event.preventDefault();
+
+    var actors = $("#actor-input").val().trim();
+
+    actor.push(actors);
+    $('#actor-input').val('');
+
+    showButtons();
+  });
+
+
+
+
+
+$(document).on("click", ".showButtons", displayActor);
+showButtons();
+
+
+
+
+
+
+
+
